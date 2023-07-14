174d07d2e7957897d74ac1387ba9d566dc9f2677
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-14 10:49:29 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,8 @@ const currentDateTime = moment().tz(desiredTimeZone);
 // Set the desired time of 10 am in the desired time zone
 const desiredTime = currentDateTime.clone().set({ hour: 10, minute: 0, second: 0 });
 
-// Calculate the delay in milliseconds from the current time to the desired time
-const delayMillis = desiredTime.valueOf() - currentDateTime.valueOf();
-
-// Schedule cron job to run after the delay
-cron.schedule(`*/45 * * * *`, () => {
+// Schedule cron job to run every 45 minutes
+cron.schedule('*/45 * * * *', () => {
   axios.get(urlHourly)
     .then((response) => {
       const responseData = response.data;
@@ -30,15 +27,13 @@ cron.schedule(`*/45 * * * *`, () => {
 });
 
 // Schedule cron job to run at the desired time in the desired time zone
-setTimeout(() => {
-  cron.schedule(`0 10 * * *`, () => {
-    axios.get(urlDaily)
-      .then((response) => {
-        const responseData = response.data;
-        console.log(`Daily URL ${urlDaily} executed successfully. Response: `, responseData);
-      })
-      .catch((error) => {
-        console.error(`An error occurred while executing Daily URL ${urlDaily}: ${error}`);
-      });
-  });
-}, delayMillis);
+cron.schedule(desiredTime.format('m H * * *'), () => {
+  axios.get(urlDaily)
+    .then((response) => {
+      const responseData = response.data;
+      console.log(`Daily URL ${urlDaily} executed successfully. Response: `, responseData);
+    })
+    .catch((error) => {
+      console.error(`An error occurred while executing Daily URL ${urlDaily}: ${error}`);
+    });
+});
