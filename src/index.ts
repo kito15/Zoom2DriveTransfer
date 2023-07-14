ff07bcaa169b08686bf61f45d4edebe0867deb9f
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-14 10:45:19 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,20 @@ import moment from 'moment-timezone';
 const urlHourly = 'https://flask-production-d5a3.up.railway.app/authorize';
 const urlDaily = 'https://flask-production-d5a3.up.railway.app';
 
-const timeZone = 'America/New_York';
-const cronExpression = moment.tz('0 10 * * *', timeZone).utc().format('m H * * *');
+// Set the desired time zone
+const desiredTimeZone = 'America/New_York';
 
-// Schedule cron job to run every 45 minutes
-cron.schedule('*/45 * * * *', () => {
+// Convert the current date and time to the desired time zone
+const currentDateTime = moment().tz(desiredTimeZone);
+
+// Set the desired time of 10 am in the desired time zone
+const desiredTime = currentDateTime.clone().set({ hour: 10, minute: 0, second: 0 });
+
+// Calculate the delay in milliseconds from the current time to the desired time
+const delayMillis = desiredTime.valueOf() - currentDateTime.valueOf();
+
+// Schedule cron job to run after the delay
+cron.schedule(`*/45 * * * *`, () => {
   axios.get(urlHourly)
     .then((response) => {
       const responseData = response.data;
@@ -20,14 +29,16 @@ cron.schedule('*/45 * * * *', () => {
     });
 });
 
-// Schedule cron job to run every day at 10 am
-cron.schedule(cronExpression, () => {
-  axios.get(urlDaily)
-    .then((response) => {
-      const responseData = response.data;
-      console.log(`Daily URL ${urlDaily} executed successfully. Response: `, responseData);
-    })
-    .catch((error) => {
-      console.error(`An error occurred while executing Daily URL ${urlDaily}: ${error}`);
-    });
-});
+// Schedule cron job to run at the desired time in the desired time zone
+setTimeout(() => {
+  cron.schedule(`0 10 * * *`, () => {
+    axios.get(urlDaily)
+      .then((response) => {
+        const responseData = response.data;
+        console.log(`Daily URL ${urlDaily} executed successfully. Response: `, responseData);
+      })
+      .catch((error) => {
+        console.error(`An error occurred while executing Daily URL ${urlDaily}: ${error}`);
+      });
+  });
+}, delayMillis);
